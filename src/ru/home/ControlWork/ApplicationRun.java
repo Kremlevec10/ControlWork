951dddccfa20979536b22aaab6065332ffd09023
Ruslan <Ruslan90.0843@gmail.com>
2023-01-30 19:23:16 +0300

    added a solution to the task

--- src/ru/home/ControlWork/ApplicationRun.java
+++ src/ru/home/ControlWork/ApplicationRun.java
@@ -1,5 +1,35 @@
 package ru.home.ControlWork;
 
+import java.util.Arrays;
+
 public class ApplicationRun {
+	
+	public static void main(String[] args) {
+		String[] array = {"Hello", "Africa", "Bus", "Bye", "Be", "Russia"};
+		
+		String[] test = serchElementMatchTask(array);
+		System.out.println(Arrays.toString(test));
+	}
+	
+	private static String[] serchElementMatchTask(String[] arr) {
+		int i = 0, count = 0;
+		while (i < arr.length) {
+			if (arr[i].length() <= 3) {
+				count ++;
+			}
+			i++;
+		}
+		String[] newArray = new String[count];
+		i = 0;
+		int indexNewArray = 0;
+		while (i < arr.length) {
+			if (arr[i].length() <= 3) {
+				newArray[indexNewArray] = arr[i];
+				indexNewArray ++;
+			}
+			i++;
+		}
+		return newArray;
+	}
 
 }
